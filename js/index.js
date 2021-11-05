fc29ddcd3bf420279ac6b9ade74cd0000ace7220
SuperCat2812 <Sashka281205@gmail.com>
2021-11-05 19:00:10 +0200

    fix

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 let p = document.getElementById("p");
 let i = document.getElementById("i");
 let b = document.getElementById("b");
-
+let k=document.getElementById("k");
 b.onclick = function () {
   console.log(i.value);
   p.innerText = i.value;
+  k.innerHTML = "<div>JS</div>";
 }
