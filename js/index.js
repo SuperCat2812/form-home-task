d0687abd9b5f0c4c760e311e2d6083d9a593e51f
SuperCat2812git config --global user.name SuperCat2812 <Sashka281205@gmail.com>
2022-09-28 19:32:11 +0200

    format

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 let p = document.getElementById("p");
 let i = document.getElementById("i");
 let b = document.getElementById("b");
-let k=document.getElementById("k");
+let k = document.getElementById("k");
 b.onclick = function () {
   console.log(i.value);
   p.innerText = i.value;
